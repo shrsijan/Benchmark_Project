b1f34f73904dc8bc3e23998d3ba721bda3bf8107
Sijan Shrestha <84898822+shrsijan@users.noreply.github.com>
2025-04-01 19:06:56 -0400

    Memory Array and Benchmark variables

--- Benchmark_memory.cpp
+++ Benchmark_memory.cpp
@@ -7,3 +7,9 @@ using namespace std;
 #define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_YELLOW "\x1b[33m"
 #define ANSI_COLOR_RESET "\x1b[0m"
+
+void memoryBenchmark() {
+    const size_t array_size = 500000000;
+    double total_time = 0.0;
+    double dummy_read_value = 0.0;
+    vector<float> my_arr(array_size, 0.0f);
