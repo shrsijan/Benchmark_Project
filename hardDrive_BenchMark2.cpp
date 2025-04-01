d776a8d116e6fcc26c3dc8e6a275535862d371e7
Sijan Shrestha <84898822+shrsijan@users.noreply.github.com>
2025-04-01 13:19:58 -0400

    Open file for writing and handle errors

--- hardDrive_BenchMark2.cpp
+++ hardDrive_BenchMark2.cpp
@@ -12,4 +12,14 @@ using namespace std;
 #define ANSI_COLOR_RESET "\x1b[0m"
 
 void hardDriveBenchmark2() {
+    const size_t total_file_size = 1000000000;
+    const size_t one_time_size = 10000;
+    double total_time = 0.0;
+
+    // Writing 10^9 bytes to a file, 10000 bytes at a time
+    ofstream output_file("dummyOneBillionBytes.txt", ios::binary);
+    if (!output_file) {
+        cerr << "Error: Could not open file for writing." << endl;
+        return;
+    }
 }
