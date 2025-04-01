4745b9157267521f51e5f3db489fd958c2a6e156
Sijan Shrestha <84898822+shrsijan@users.noreply.github.com>
2025-03-31 22:42:52 -0400

    Define constants and initialize benchmark function

--- hardDrive_BenchMark1.cpp
+++ hardDrive_BenchMark1.cpp
@@ -9,3 +9,9 @@ using namespace std;
 #define ANSI_COLOR_RED "\x1b[31m"
 #define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+void mainBenchmark() {
+    const size_t total_file_size = 1000000000;
+    const size_t one_time_size = 100;
+
