558095dbc254957f7f4b285e1c6fdb0e1b9dbe6f
Sijan Shrestha <84898822+shrsijan@users.noreply.github.com>
2025-03-31 21:49:39 -0400

    Implement function mainBenchmark() and initialize variables

--- float_benchmark.cpp
+++ float_benchmark.cpp
@@ -6,3 +6,13 @@
 #define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_YELLOW "\x1b[33m"
 #define ANSI_COLOR_RESET "\x1b[0m"
+
+
+void mainBenchmark() {
+    double total_time = 0.0;
+    double dummy_value = 8.4;
+    double res = 1.2;
+}
+
+    // Start benchmarking additions
+
