096b65205e000f40c1392b1ad23a927d6afacc59
Sijan Shrestha <84898822+shrsijan@users.noreply.github.com>
2025-03-31 21:44:14 -0400

    Define ANSI escape codes for colors

---
+++ float_benchmark.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <chrono>
+
+// ANSI escape codes for colors
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_RESET "\x1b[0m"
